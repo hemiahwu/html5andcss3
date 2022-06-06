109ee30122c3eafa9507becb2e6e18ef92b3b8e9
hemiahwu <309595700@qq.com>
2022-06-06 16:47:58 +0800

    h5

--- 05-css3专题代码/12css-var/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-const box = document.querySelector(".box");
-const boxStyles = getComputedStyle(box);
-// console.log(boxStyles);
-const boxBorderColor = boxStyles.getPropertyValue("--box-border-color");
-// console.log(boxBorderColor);
-
-const header = document.querySelector("#main-header");
-header.style.setProperty("--header-bg-color", "green");
